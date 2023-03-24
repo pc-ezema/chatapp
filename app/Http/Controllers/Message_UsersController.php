6dcf87846cf20c20a713341c3bfa41c80db078d4
Promise <promiseezema11@gmail.com>
2023-03-24 08:44:52 +0100

    updated

--- app/Http/Controllers/Message_UsersController.php
+++ app/Http/Controllers/Message_UsersController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Auth;
 use App\Message_Users;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
 
 class Message_UsersController extends Controller
 {
     public function check($recieverId){
-        $senderId = Auth::user()->id;
+        $senderId = FacadesAuth::user()->id;
 
         $data = [
             'sender_id' => $senderId,
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Auth::routes();
 
 Route::group(['middleware' => 'guest'],function(){ 
